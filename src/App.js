f372a042743cf2ecaae32085058441ec2884d93f
Kyle <kylehorton33@gmail.com>
2020-07-15 20:18:06 -0400

    video 60: connected to API!

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import './App.css';
 
 function App() {
+
+  const [movies, setMovie] = useState([]);
+
+  useEffect( () => {
+      fetch("http://127.0.0.1:8000/api/movies/", { 
+        method : 'GET',
+        headers : {
+          'Content-Type' : 'application/json',
+          'Authorization' : 'Token a4db63b236737f5d5fe06163a57b83a797ab55e7'
+        }
+       })
+       .then( resp => resp.json())
+       .then( resp => setMovie(resp))
+       .catch( err => console.log(err))
+  }, [] )
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,7 +26,11 @@ function App() {
         
       </header>
       <div className="layout">
-          <div>List</div>
+          <div>
+            { movies.map( movie => {
+              return <h2>{ movie.title }</h2>
+            }) }
+          </div>
           <div>Details</div>
       </div>
     </div>
